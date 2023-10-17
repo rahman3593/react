ee01c5a792911da1804108220769fe1e9f0f5a44
Abdul Rahman <abdulrahmana3593@gmail.com>
2023-10-17 22:26:30 +0530

    AR: Episode-01

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent= React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"Hello World from React! h1"),
+        React.createElement("h2",{},"Hello World from React! h2")
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"Hello World from React! h1"),
+        React.createElement("h2",{},"Hello World from React! h2")
+    ])
+])
+
+ const root= ReactDOM.createRoot(document.getElementById("root"));
+ root.render(parent);
