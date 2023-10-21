0c31c463a3598e41a109fca074e465728a42752e
Abdul Rahman <abdulrahmana3593@gmail.com>
2023-10-21 21:16:42 +0530

    AR: Episode-02

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-const parent= React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"Hello World from React! h1"),
-        React.createElement("h2",{},"Hello World from React! h2")
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child1"}, [
+        React.createElement("h1", {}, "This is abdul"),
+        React.createElement("h2", {}, "Hello World from React! h2")
     ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"Hello World from React! h1"),
-        React.createElement("h2",{},"Hello World from React! h2")
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {}, "Hello World from React! h1"),
+        React.createElement("h2", {}, "Hello World from React! h2")
     ])
 ])
 
- const root= ReactDOM.createRoot(document.getElementById("root"));
- root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
