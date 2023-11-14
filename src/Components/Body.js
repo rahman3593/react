05969804ad0983d9bbb27c109f1075aa5534a4f0
Abdul Rahman <abdulrahmana3593@gmail.com>
2023-11-14 12:07:53 +0530

    AR: Episode-05

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import RestaurantCard from "./RestaurantCard";
 import restList from "../Utils/mockData";
 
 const Body = () => {
+    const [listRestaurants, setListRestaurants] = useState(restList)
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    const filteredRestaurants = listRestaurants.filter((rest) => rest.info.avgRating > 4);
+                    setListRestaurants(filteredRestaurants);
+                }}>Top Rated Restaurants
+                </button>
+            </div>
             <div className="res-container">
-                {restList.map(restaurant =>
+                {listRestaurants.map(restaurant =>
                     <RestaurantCard key={restaurant.info.id} restObj={restaurant}/>
                 )}
             </div>
